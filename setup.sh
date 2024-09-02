a0a5cfc2a2147203729c9ab69687d3f40018c12b
AmirparsaDD <Amirparsab9@gmail.com>
2024-09-02 13:00:15 +0330

    made it look cleaner

--- setup.sh
+++ setup.sh
@@ -9,9 +9,9 @@ iptables -A INPUT -p tcp --dport 25565 -j ACCEPT
 # Add Plugins
 cd plugins
 curl https://plixhost-cdn.s3.ir-thr-at1.arvanstorage.ir/binaries%2Fautoupdateplugins-9.5.jar >> AutoUpdate.jar
+cd ..
 
 # Setup The Server
-cd ..
 # Replace With The Requested Version
 curl https://api.papermc.io/v2/projects/paper/versions/1.21.1/builds/17/downloads/paper-1.21.1-17.jar >> server.jar
 
